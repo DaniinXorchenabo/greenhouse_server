371680cac83789ab5b0e1b24151f83e72bebfa83
DaniinXorchenabo <rkbcu@mail.ru>
2021-09-15 19:12:04 +0300

    add auto migration system

--- app/src/db/piccolo_conf.py
+++ app/src/db/piccolo_conf.py
@@ -4,13 +4,17 @@ if os.environ.get("PG_SUPERUSER_NAME") is None:
     from dotenv import load_dotenv
 
 
+    path = dirname(__file__)
+    while "app" in (path := split(path)[0]):
+        print(path)
 
-    dotenv_path = join(split(split(split(dirname(__file__))[0])[0])[0], '.env')
+    dotenv_path = join(path, '.env')
+    print(dotenv_path, __file__)
     if os.path.exists(dotenv_path):
         load_dotenv(dotenv_path)
     else:
         print("В корне проекта обязательно должен быть файл .env!!!!")
-        FileNotFoundError("\n.env file must be in project root\n")
+        raise FileNotFoundError("\n.env file must be in project root\n")
 
 
 # project_dir = split(split(dirname(__file__))[0])[0]
@@ -20,30 +24,39 @@ from piccolo.engine.postgres import PostgresEngine
 
 from piccolo.conf.apps import AppRegistry
 
-print("-------------", os.environ.get("PG_SUPERUSER_NAME"), __name__)
+print("%%%%%-------------", os.environ.get("PG_SUPERUSER_NAME"), __name__)
 DB = PostgresEngine(
     config={
         "database": os.environ.get("PGDATABASE"),
         "user": os.environ.get("PG_SUPERUSER_NAME"),
         "password": os.environ.get("PG_SUPERUSER_PASSWORD"),
+        "host": "host.docker.internal", # os.environ.get("PGHOST"),
+        "port": os.environ.get("PGPORT"),
+    }
+)
+print("***%%%%%-------------", os.environ.get("PG_SUPERUSER_NAME"), __name__)
+
+readonly_DB = PostgresEngine(
+    config={
+        "database": os.environ.get("PGDATABASE"),
+        "user": os.environ.get("PG_GUEST_NAME"),
+        "password": os.environ.get("PG_GUEST_PASSWORD"),
         "host": os.environ.get("PGHOST"),
         "port": os.environ.get("PGPORT"),
     }
 )
 
-# DB = PostgresEngine(
-#     config={
-#         "database": "greenhouse_server_db" or os.environ.get("PGDATABASE"),
-#         "user": "admin" or os.environ.get("PG_SUPERUSER_NAME"),
-#         "password": "12345678" or os.environ.get("PG_SUPERUSER_PASSWORD"),
-#         "host": "localhost" or os.environ.get("PGHOST"),
-#         "port": "5432" or os.environ.get("PGPORT"),
-#     }
-# )
-
 
 # import src.db.gh.piccolo_app
+# piccolo_conf = os.environ.pop("PICCOLO_CONF", None)
+# APP_REGISTRY = AppRegistry(
+#     apps=[
+#         "gh.piccolo_app",
+#         "piccolo_admin.piccolo_app"
+#     ]
+# )
 try:
+    print("!!!!!!!!!!!_-_---------------------------")
     APP_REGISTRY = AppRegistry(
         apps=[
             "src.db.gh.piccolo_app",
@@ -51,9 +64,10 @@ try:
         ]
     )
 
-except ModuleNotFoundError as e:
-    print("$$$$$$$$$$$$$$$$$", e, [e])
+except Exception as e:
+    print(e)
     piccolo_conf = os.environ.pop("PICCOLO_CONF", None)
+    print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
     APP_REGISTRY = AppRegistry(
         apps=[
             "gh.piccolo_app",
