572eea57398bf1f25da20d743db29764b66a05f0
DaniinXorchenabo <rkbcu@mail.ru>
2021-09-15 19:12:53 +0300

    add testing postgres users access

--- app/src/init_app.py
+++ app/src/init_app.py
@@ -1,10 +1,14 @@
+import asyncpg
 from piccolo.engine import engine_finder
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 
 from src.api.add_routers import add_routers_func
+from src.db.piccolo_conf import readonly_DB
 
 
 
-def init_app_func(app):
+def init_app_func(app: FastAPI):
     """Добавление событий, обработчиков исключений
 
     Добавление всего того, что можно добавить только к FastApi()
@@ -17,6 +21,7 @@ def init_app_func(app):
         try:
             engine = engine_finder()
             await engine.start_connection_pool()
+            await readonly_DB.start_connection_pool()
         except Exception:
             print("Unable to connect to the database")
 
@@ -25,8 +30,25 @@ def init_app_func(app):
         try:
             engine = engine_finder()
             await engine.close_connection_pool()
+            await readonly_DB.close_connection_pool()
+
         except Exception:
             print("Unable to connect to the database")
+
+
+    @app.exception_handler(asyncpg.exceptions.InsufficientPrivilegeError)
+    async def permission_error(request: Request, exc: asyncpg.exceptions.InsufficientPrivilegeError):
+        """asyncpg.exceptions.InsufficientPrivilegeError: permission denied for table <my table>
+
+        Вызывается тогда, когда пользователю базы данных не доступна
+        запрашиваемая операция. К примеру, пользователь БД read-only
+        (может выполнять только SELECT)
+        попытался выполнить операцию SQL INSERT
+        """
+        return JSONResponse(
+            status_code=400,
+            content={"message": f"Вы не имеете доступа, необходимого для выполнения запроса"},
+        )
 
 
 
