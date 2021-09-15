d3ee7a2ea7852fd9937ff696bfa635bf14b4619a
DaniinXorchenabo <rkbcu@mail.ru>
2021-09-15 19:07:08 +0300

    change import .env file

--- app/main.py
+++ app/main.py
@@ -1,19 +1,35 @@
 import os
-if os.environ.get("PG_SUPERUSER_NAME") is None:
-    from dotenv import load_dotenv
-    from os.path import dirname,  join, split
+from dotenv import load_dotenv
+from os.path import dirname, join, split
 
-    dotenv_path = join((split(dirname(__file__))[0]), '.env')
+
+if os.environ.get("PG_SUPERUSER_NAME") is None:
+
+    path = dirname(__file__)
+    while "app" in (path := split(path)[0]):
+        pass
+
+    dotenv_path = join(path, '.env')
+    print(dotenv_path, __file__)
     if os.path.exists(dotenv_path):
         load_dotenv(dotenv_path)
     else:
         print("В корне проекта обязательно должен быть файл .env!!!!")
-        FileNotFoundError("\n.env file must be in project root\n")
+        raise FileNotFoundError("\n.env file must be in project root\n")
+
+path = dirname(__file__)
+while "app" in (path := split(path)[0]):
+    pass
+path = join(path, "app", "src", "db", "picolo_conf.env")
+# os.environ["PICCOLO_CONF"] = "src.db.piccolo_conf"
+if os.path.exists(path):
+    load_dotenv(path)
+
 
 from fastapi import FastAPI
 import uvicorn
 from src.init_app import init_app_func
-
+import importlib
 app = FastAPI()
 
 
