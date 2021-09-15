eee30fc082f226972e8a268afaa3d11fb4607585
DaniinXorchenabo <rkbcu@mail.ru>
2021-09-15 19:08:39 +0300

    change .env import

--- app/src/db/create_picolo_users.py
+++ app/src/db/create_picolo_users.py
@@ -1,17 +1,25 @@
 import os
+
+
 if os.environ.get("PG_SUPERUSER_NAME") is None:
     from dotenv import load_dotenv
     from os.path import dirname,  join, split
 
-    dotenv_path = join(split(split(split(dirname(__file__))[0])[0])[0], '.env')
+    path = dirname(__file__)
+    while "app" in (path := split(path)[0]):
+        pass
+
+    dotenv_path = join(path, '.env')
+    print(dotenv_path, __file__)
     if os.path.exists(dotenv_path):
         load_dotenv(dotenv_path)
     else:
         print("В корне проекта обязательно должен быть файл .env!!!!")
-        FileNotFoundError("\n.env file must be in project root\n")
+        raise FileNotFoundError("\n.env file must be in project root\n")
 
 
 from piccolo.apps.user.tables import BaseUser
+
 
 BaseUser(username=os.environ.pop("PICOLO_SUPERUSER_LOGIN"),
          password=os.environ.pop("PICOLO_SUPERUSER_PASSWORD"),
