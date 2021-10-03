09fe128326495873e1d07bfc4a3ce01cc59ab8a4
DaniinXorchenabo <rkbcu@mail.ru>
2021-10-03 20:25:08 +0300

    refactoring migration system

--- app/src/db/_migrations/gh_real/1_20211003201548_create_user.sql
+++ app/src/db/_migrations/gh_real/1_20211003201548_create_user.sql
@@ -1,5 +1,6 @@
 -- upgrade --
 CREATE TABLE IF NOT EXISTS "user" (
+    "_scopes" VARCHAR(50) NOT NULL  DEFAULT '',
     "id" UUID NOT NULL  PRIMARY KEY,
     "username" VARCHAR(50) NOT NULL UNIQUE,
     "name" VARCHAR(100),
@@ -7,5 +8,6 @@ CREATE TABLE IF NOT EXISTS "user" (
     "hashed_password" VARCHAR(4096) NOT NULL,
     "email" VARCHAR(100) NOT NULL UNIQUE
 );
+COMMENT ON COLUMN "user"."_scopes" IS 'Разрешения для пользователя, приписанные нотацией OAuth2';
 -- downgrade --
 DROP TABLE IF EXISTS "user";
