71a5518f5838170ad4c2daf865fcc62aa601a8df
DaniinXorchenabo <rkbcu@mail.ru>
2021-10-14 20:47:11 +0300

    added CRUD operations

--- app/src/sqlalchemy/db/orm_model_expansion/user.py
+++ app/src/sqlalchemy/db/orm_model_expansion/user.py
@@ -1,18 +1,15 @@
 from uuid import UUID
-from typing import Awaitable, Optional, Any, Union, AsyncIterator
+from typing import Awaitable, Optional, Any
 
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy import update as sqlalchemy_update
+from sqlalchemy import update as sqlalchemy_update, delete
 from sqlalchemy.future import select
-from sqlalchemy.orm import relationship
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.sql.selectable import Select
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine.result import ChunkedIteratorResult, Result
 
 
 from src.utils.enums import Scopes
-from src.sqlalchemy.db.schemes._real import CreateUser
-
 
 
 class ScopesForUser(object):
@@ -38,20 +35,48 @@ class MapperOfUser(object):
         await session.commit()
 
     @classmethod
-    async def get_via_username(
-            cls,
-            _session: sessionmaker,
-            username: str) -> Awaitable[Optional[Any]]:
-
+    async def get_via_username(cls, _session: sessionmaker, username: str) -> Awaitable[Optional[Any]]:
         async with _session() as session:
-
+            session: AsyncSession
             query: Select = select(cls).where(cls.username == username)
-            res = await session.execute(query)
+            res: Result = await session.execute(query)
+            print(type(res))
             user_ = res.scalars().first()
             print("**_------------", user_, [user_], type(user_))
             return user_
 
     @classmethod
+    async def get_via_id(cls, _session: sessionmaker, id_: UUID) -> Awaitable[Optional[Any]]:
+        async with _session() as session:
+            session: AsyncSession
+            query: Select = select(cls).where(cls.id == id_)
+            res: Result = await session.execute(query)
+            user_ = res.scalars().first()
+            return user_
+
+    @classmethod
+    async def all(cls, _session: sessionmaker):
+        print(f'get all {cls.__name__}')
+
+        async with _session() as session:
+            session: AsyncSession
+            res: Result = await session.execute(select(cls))
+
+            users = res.scalars().all()
+            print("**_------------", users, type(users))
+            return users
+
+    @classmethod
     def create(cls, session: AsyncSession, **kwargs) -> Awaitable:
         session.add(cls(**kwargs))
         return session.commit()
+
+    def update_myself(self, session: AsyncSession, values: dict[str, Any]):
+        q = sqlalchemy_update(self.__class__).where(self.__class__.id == self.id).values(**values)
+        q.execution_options(synchronize_session="fetch")
+        return session.execute(q)
+
+    @classmethod
+    def delete(cls, session: AsyncSession, id_: UUID) -> Awaitable:
+        q = delete(cls).where(cls.id == id_)
+        return session.execute(q)
